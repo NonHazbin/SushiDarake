ff131542a444368efd832b8093c220f30de8fb91
NonHazbin <non_hazbin@icloud.com>
2025-08-10 17:23:21 +0900

    GameManagerの修正

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -10,11 +10,11 @@ public class GameManager : MonoBehaviour
 
     public GameObject _tilePrefab; //生成するタイル
 
-    private Vector2 touchStartPos; //スワイプ
-    private float swipeThershold = 80f; // スワイプを検出するための閾値
+    private Vector2 _touchStartPos; //スワイプ
+    private float _swipeThershold = 80f; // スワイプを検出するための閾値
 
-    private bool canPlay = true;
-    private bool tapped = false;
+    private bool _canPlay = true;
+    private bool _tapped = false;
 
     private void Start()
     {
@@ -25,14 +25,26 @@ public class GameManager : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetMouseButtonDown(0) && canPlay)
+        if (Input.GetMouseButtonDown(0) && _canPlay)
         {
-            touchStartPos = Input.mousePosition;
-            tapped = true;
+            _touchStartPos = Input.mousePosition;
+            _tapped = true;
         }
-        else if (Input.GetMouseButton(0) && canPlay && tapped)
+        else if (Input.GetMouseButton(0) && _canPlay && _tapped)
         {
-            
+            Vector2 touchEndPos = Input.mousePosition;
+
+            float swipeX = touchEndPos.x - _touchStartPos.x;
+            float swipeY = touchEndPos.y - _touchStartPos.y;
+
+            if (Mathf.Abs(swipeX) > Mathf.Abs(swipeY))
+            {
+                if (swipeX > _swipeThershold)
+                {
+                    _tapped = false;
+                    RightSwipe();
+                }
+            }
         }
     }
 
@@ -46,7 +58,7 @@ public class GameManager : MonoBehaviour
             for (int j = 0; j < FOUR; j++)
             {
                 _squareArray[i, j] = _squares[count];
-                //_squareArray[i,j].GetComponent<Square>().tileValue = 0;
+                _squareArray[i,j].GetComponent<Square>().tileValue = 0;
                 count++;
             }
         }
@@ -65,8 +77,9 @@ public class GameManager : MonoBehaviour
         newTileRect.position = emptyCell.GetComponent<RectTransform>().position;
 
         int newValue = Random.Range(1, 3) * 2;
-
         newTileRect.DOScale(Vector3.one, 0.1f).SetLink(newTile);
+
+        newTile.GetComponent<Tile>().SetValue(newValue);
 
         emptyCell.GetComponent<Square>().tileValue = newTile.GetComponent<Tile>()._tileValue;
         emptyCell.GetComponent<Square>().tileOnMe = newTile;
@@ -90,5 +103,66 @@ public class GameManager : MonoBehaviour
             return selectedSquare;
         }
         return null;
+    }
+
+    private void RightSwipe()
+    {
+        bool canMove = false;
+
+        for (int row = 0; row < 4; row++)
+        {
+            for (int column = 3; column >= 0; column--)
+            {
+                Debug.Log(_squareArray[row, column].GetComponent<Square>().tileValue);
+                int myNumber;
+                if (_squareArray[row, column].GetComponent<Square>().tileValue != 0)
+                {
+                    GameObject temp = _squareArray[row, column].GetComponent<Square>().tileOnMe;
+                    myNumber = _squareArray[row, column].GetComponent<Square>().tileValue;
+
+                    int x = column;
+                    while (x < 3 && _squareArray[row, x + 1].GetComponent<Square>().tileValue == 0)
+                    {
+                        canMove = true;
+                        x++;
+                    }
+
+                    if (!canMove)
+                    {
+                        continue;
+                    }
+                    GameObject targetSquare = _squareArray[row, x];
+                    targetSquare.GetComponent<Square>().tileValue = myNumber;
+
+                    EmptyMySquare(_squareArray[row, column]);
+
+                    TileMoveAnimation(temp, targetSquare);
+
+                    targetSquare.GetComponent<Square>().tileOnMe = temp;
+
+                }
+            }
+        }
+        if (canMove)
+        {
+            _canPlay = false;
+            Invoke("SpawnTile", 0.2f);
+        }
+    }
+
+    // 元いたマスを空っぽにする
+    private void EmptyMySquare(GameObject mySquare_)
+    {
+        mySquare_.GetComponent<Square>().tileValue = 0;
+        mySquare_.GetComponent<Square>().tileOnMe = null;
+    }
+    // タイルの移動
+    private void TileMoveAnimation(GameObject temp_, GameObject targetSquare_)
+    {
+        RectTransform tempRect = temp_.GetComponent<RectTransform>();
+        tempRect.SetParent(targetSquare_.transform);
+
+        Vector2 endPos = targetSquare_.GetComponent<RectTransform>().position;
+        tempRect.DOAnchorPos(endPos, 0.1f).SetEase(Ease.Linear).SetLink(temp_);
     }
 }
